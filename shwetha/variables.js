c840df6e2e6f5141db883655c2dac8170e06fcdd
ShwethaMuktha <shwethakshet16@gmail.com>
2026-04-11 17:17:57 +0530

    adding variables section to git

---
+++ shwetha/variables.js
@@ -0,0 +1,21 @@
+var fname="shwetha";
+fname="muktha";
+var fname="shet";
+ 
+var lname="s";
+var age=22;
+var isMarried=false;
+console.log(fname);
+console.log(lname);
+console.log(age);
+console.log(isMarried);
+
+//invalid variable names
+//var 1name="shwetha";
+//var @name="shwetha";
+//var #name="shwetha";
+//var *name="shwetha";
+//var -name="shwetha";  
+
+//valid variable names  
+var name1="shwetha";
